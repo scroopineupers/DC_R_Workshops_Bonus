74028a17b62012e2f38b056afe1de60a6ca20006
scroopineupers <colemaaj@gmail.com>
2018-01-18 11:28:55 -0600

    set up a few basic plots...we're not very useful

--- bonus_project.R
+++ bonus_project.R
@@ -9,3 +9,18 @@ full_dates = ISOdate(surveys$year, surveys$month, surveys$day, tz = Sys.timezone
 
 # remove time stamps from full_dates and appends data.frame with 'date' column
 surveys$date = as.Date(full_dates)
+
+# now to start some ggplots...
+ggplot(surveys, aes(x=date, y=weight)) +
+  geom_line()
+  # maybe a line graph isn't the best...
+
+# let's try out a scatterpot
+ggplot(surveys, aes(x=date, y=weight)) +
+  geom_point()
+  # that looks better, but you can do even better
+
+# color the scatterplot points by date
+ggplot(surveys, aes(x=date, y=weight)) +
+  geom_point(aes(color=date))
+  # not too shabby, still kind of intelligable
